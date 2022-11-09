8216e39192aa0ff0a2c42ebf840d132f36dfd894
Akilandeshwari-Ravishankar <akilandeshwari.ravishankar@gmail.com>
2022-11-09 22:40:47 +0530

    updated smallchange.sql

--- SmallChange/src/main/resources/smallchange.sql
+++ SmallChange/src/main/resources/smallchange.sql
@@ -88,9 +88,9 @@ INSERT INTO SC_CLIENT_IDENTIFICATION ("client_id", "type", "value") VALUES ('ea0
 INSERT INTO SC_INSTRUMENT("instrument_id","instrument_description","external_id","external_id_type","min_quantity","max_quantity","category_id") VALUES ('T67897','USA, Note 2.5 31jan2021 2Y','9128285X4','CUSIP',100,1000,'GOVT');
 INSERT INTO SC_PRICE ("instrument_id","bid_price","ask_price","timestamp") VALUES ('T67897',0.99828125,0.998125,TO_DATE('2022-01-22', 'yyyy-mm-dd'));
 INSERT INTO SC_ACCOUNT ("client_id", "account_number", "max_accounts") VALUES ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515', 'abcd-efgh', 4);
-INSERT INTO SC_HOLDING ("client_id", "instrument_id", "direction", "no_of_shares") values ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515','T67897','B','1');
+INSERT INTO SC_HOLDING ("client_id", "instrument_id", "direction", "no_of_shares") values ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515','T67897','Buy','1');
 INSERT INTO SC_PREFERENCES("client_id", "investment_purpose", "risk_tolerance", "income_category", "length_of_investment") VALUES ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515','Savings', 'Average', '20000-60000', '5-7years');
-INSERT INTO SC_TRADE_HISTORY ("client_id","cash_value","direction","trade_id","instrument_id","quantity","execution_price","trade_timestamp") VALUES('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515',500,'B','T123456','T67897',1,300,null);
+INSERT INTO SC_TRADE_HISTORY ("client_id","cash_value","direction","trade_id","instrument_id","quantity","execution_price","trade_timestamp") VALUES('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515',500,'Buy','ff7f0b15-5e09-4a4e-123a-6bc64035b73f','T67897',1,300, CURRENT_TIMESTAMP);
 
 create or replace PROCEDURE proc_delete_instrument_delete_holding (
 	parm_instrument_to_delete IN VARCHAR2
