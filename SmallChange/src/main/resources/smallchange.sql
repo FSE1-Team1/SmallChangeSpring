14c6c74f9814dfaec89d15636735728bee31c44f
MakineediSaiHarsh <msaiharsh@gmail.com>
2022-11-10 14:23:47 +0530

    Update smallchange.sql

--- SmallChange/src/main/resources/smallchange.sql
+++ SmallChange/src/main/resources/smallchange.sql
@@ -83,14 +83,28 @@ ALTER TABLE SC_PREFERENCES ADD CONSTRAINT SC_PREFERENCES_FK FOREIGN KEY ("client
 ALTER TABLE SC_TRADE_HISTORY ADD CONSTRAINT SC_TRADE_HISTORY_FK FOREIGN KEY ("client_id") REFERENCES SC_CLIENT ("client_id") ENABLE;
 ALTER TABLE SC_PRICE ADD CONSTRAINT SC_PRICE_FK FOREIGN KEY ("instrument_id") REFERENCES SC_INSTRUMENT ("instrument_id") ENABLE;
 
-INSERT INTO SC_CLIENT ("client_id", "first_name", "last_name", "dob", "email", "password", "country", "postal_code") VALUES ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515', 'ZTI', '1', to_date('2001-MAR-03', 'YYYY-MON-DD'), 'zti@gmail.com', 'Testing123*','IN', '18181818');
+INSERT INTO SC_CLIENT ("client_id", "first_name", "last_name", "dob", "email", "password", "country", "postal_code") VALUES ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515', 'ZTI', '1', '', 'zti@gmail.com', 'Testing123*','IN', '18181818');
 INSERT INTO SC_CLIENT_IDENTIFICATION ("client_id", "type", "value") VALUES ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515', 'Passport', '10101010');
 INSERT INTO SC_INSTRUMENT("instrument_id","instrument_description","external_id","external_id_type","min_quantity","max_quantity","category_id") VALUES ('T67897','USA, Note 2.5 31jan2021 2Y','9128285X4','CUSIP',100,1000,'GOVT');
 INSERT INTO SC_PRICE ("instrument_id","bid_price","ask_price","timestamp") VALUES ('T67897',0.99828125,0.998125,TO_DATE('2022-01-22', 'yyyy-mm-dd'));
 INSERT INTO SC_ACCOUNT ("client_id", "account_number", "max_accounts") VALUES ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515', 'abcd-efgh', 4);
-INSERT INTO SC_HOLDING ("client_id", "instrument_id", "direction", "no_of_shares") values ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515','T67897','Buy','1');
+INSERT INTO SC_HOLDING ("client_id", "instrument_id", "direction", "no_of_shares") values ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515','T67897','B','1');
 INSERT INTO SC_PREFERENCES("client_id", "investment_purpose", "risk_tolerance", "income_category", "length_of_investment") VALUES ('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515','Savings', 'Average', '20000-60000', '5-7years');
-INSERT INTO SC_TRADE_HISTORY ("client_id","cash_value","direction","trade_id","instrument_id","quantity","execution_price","trade_timestamp") VALUES('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515',500,'Buy','ff7f0b15-5e09-4a4e-123a-6bc64035b73f','T67897',1,300, CURRENT_TIMESTAMP);
+INSERT INTO SC_TRADE_HISTORY ("client_id","cash_value","direction","trade_id","instrument_id","quantity","execution_price","trade_timestamp") VALUES('ea0dd5f8-51b8-40b4-ab1e-a386a1c2c515',500,'B','T123456','T67897',1,300,null);
+
+INSERT INTO SC_INSTRUMENT("instrument_id","instrument_description","external_id","external_id_type","min_quantity","max_quantity","category_id") VALUES ('APP','USA, Note 2.5 31jan2021 2Y','9128285X4','CUSIP',1000,10000,'GOVT');
+INSERT INTO SC_PRICE ("instrument_id","bid_price","ask_price","timestamp") VALUES ('APP',2.5,3.5,TO_DATE('2022-01-22', 'yyyy-mm-dd'));
+INSERT INTO SC_INSTRUMENT("instrument_id","instrument_description","external_id","external_id_type","min_quantity","max_quantity","category_id") VALUES ('Google','USA, Note 2.5 31jan2021 2Y','9128285X4','CUSIP',1,100000,'GOVT');
+INSERT INTO SC_PRICE ("instrument_id","bid_price","ask_price","timestamp") VALUES ('Google',0.05,5.5,TO_DATE('2022-01-22', 'yyyy-mm-dd'));
+
+INSERT INTO SC_INSTRUMENT("instrument_id","instrument_description","external_id","external_id_type","min_quantity","max_quantity","category_id") VALUES ('Tata','USA, Note 2.5 31jan2021 2Y','9128285X4','CUSIP',500,5000,'GOVT');
+INSERT INTO SC_PRICE ("instrument_id","bid_price","ask_price","timestamp") VALUES ('Tata',0.1,1.5,TO_DATE('2022-01-22', 'yyyy-mm-dd'));
+
+INSERT INTO SC_INSTRUMENT("instrument_id","instrument_description","external_id","external_id_type","min_quantity","max_quantity","category_id") VALUES ('Samsung','USA, Note 2.5 31jan2021 2Y','9128285X4','CUSIP',50,500000,'GOVT');
+INSERT INTO SC_PRICE ("instrument_id","bid_price","ask_price","timestamp") VALUES ('Samsung',4.0,4.5,TO_DATE('2022-01-22', 'yyyy-mm-dd'));
+
+INSERT INTO SC_INSTRUMENT("instrument_id","instrument_description","external_id","external_id_type","min_quantity","max_quantity","category_id") VALUES ('TZW','USA, Note 2.5 31jan2021 2Y','9128285X4','CUSIP',5,1500,'GOVT');
+INSERT INTO SC_PRICE ("instrument_id","bid_price","ask_price","timestamp") VALUES ('TZW',0.34,0.5,TO_DATE('2022-01-22', 'yyyy-mm-dd'));
 
 create or replace PROCEDURE proc_delete_instrument_delete_holding (
 	parm_instrument_to_delete IN VARCHAR2
